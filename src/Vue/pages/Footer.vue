9997126def7d3ff1608e3189ebdda378e19854f9
katarinahudakova <72190543+katarinahudakova@users.noreply.github.com>
2022-05-26 23:34:40 +0200

    create footer

---
+++ src/Vue/pages/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer font-small text-center">
+    <div class="row">
+      <div class="logo">
+        <img src="../../assets/logo.png" alt="" />
+      
+
+      <div class="copyright">
+        <p>Copyright © {{ new Date().getFullYear() }}</p>
+      </div>
+    </div>
+    </div>
+  </footer>
+</template>
