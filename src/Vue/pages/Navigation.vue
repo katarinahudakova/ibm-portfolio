b3ee71f7f5716400587089180950803bb837a14a
katarinahudakova <72190543+katarinahudakova@users.noreply.github.com>
2022-05-26 23:33:56 +0200

    create navigation

---
+++ src/Vue/pages/Navigation.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <cv-header aria-label="Carbon header">
+    <cv-header-name href="#" prefix="IBM">
+      &nbsp;[Automation Team Portfolio]
+    </cv-header-name>
+
+    <cv-header-nav aria-label="Carbon nav" class="navi">
+      <cv-header-menu-item href="#about">
+        About us
+      </cv-header-menu-item>
+      <cv-header-menu-item href="javascript:void(0)">
+        Projects
+      </cv-header-menu-item>
+      <cv-header-menu-item href="#contact">
+        Contact us
+      </cv-header-menu-item>
+      <cv-header-menu-item href="#" class="home-icon" active>
+          <i class="fa fa-home"></i>
+      </cv-header-menu-item>
+    </cv-header-nav>
+  </cv-header>
+
+
+  
+</template>
+
