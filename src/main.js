a5c8fc66acd8daf00082f2fea7d3b9ae6fd51607
katarinahudakova <72190543+katarinahudakova@users.noreply.github.com>
2022-05-26 23:38:52 +0200

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* importovanie carbonu z ibm */
+import 'carbon-components/css/carbon-components.css';
+import CarbonComponentsVue from '@carbon/vue';
+Vue.use(CarbonComponentsVue);
+
+/* importovanie bootstrapu */
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'bootstrap-icons/font/bootstrap-icons'
+
+
+
+
+/* importovanie font awesome icony */
+/* add fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas, fab);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+
+
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
