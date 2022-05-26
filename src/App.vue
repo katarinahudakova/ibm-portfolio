cc4b15de196dbb1b54a56509365f9039dfa0be4f
katarinahudakova <72190543+katarinahudakova@users.noreply.github.com>
2022-05-26 23:43:05 +0200

    updated app vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navigation />
+    <Hero />
+    <!-- <div class="sections-together"> -->
+      <About />
+      <Projects />
+      <Contact />
+    <!-- </div> -->
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './Vue/pages/Navigation.vue'
+import Hero from './Vue/pages/Hero.vue'
+import About from './Vue/pages/About.vue'
+import Projects from './Vue/pages/Projects.vue'
+import Contact from './Vue/pages/Contact.vue'
+import Footer from './Vue/pages/Footer.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navigation,
+    Hero,
+    About,
+    Projects,
+    Contact,
+    Footer
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
+
+@import "./style/scss/main.css";
+
 </style>
