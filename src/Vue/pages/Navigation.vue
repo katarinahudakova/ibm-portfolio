4727d2ad3b2965123e8d79e7b5bbd3602548a55c
katarinahudakova <72190543+katarinahudakova@users.noreply.github.com>
2022-05-27 13:07:36 +0200

    responsive navigation

--- src/Vue/pages/Navigation.vue
+++ src/Vue/pages/Navigation.vue
@@ -1,6 +1,11 @@
 <template>
 
   <cv-header aria-label="Carbon header">
+    <cv-header-menu-button aria-label="Header menu" aria-controls="side-nav" />
+    <cv-skip-to-content href="#main-content">
+      Skip to content
+    </cv-skip-to-content>
+    
     <cv-header-name href="#" prefix="IBM">
       &nbsp;[Automation Team Portfolio]
     </cv-header-name>
@@ -19,9 +24,29 @@
           <i class="fa fa-home"></i>
       </cv-header-menu-item>
     </cv-header-nav>
-  </cv-header>
 
+    <cv-side-nav id="side-nav" fixed>
+    <cv-side-nav-items>
+      <cv-header-side-nav-items>
 
-  
+        <cv-header-menu-item href="#about">
+          About us
+        </cv-header-menu-item>
+        <cv-header-menu-item href="#projects">
+          Projects
+        </cv-header-menu-item>
+        <cv-header-menu-item href="#contact">
+          Contact us
+        </cv-header-menu-item>
+        
+
+    </cv-header-side-nav-items>
+    </cv-side-nav-items>
+  </cv-side-nav>
+    <template v-slot:right-panels v-if="areRightPanels"></template>
+
+  </cv-header> 
+
+   
 </template>
 
