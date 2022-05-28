9186155fc2244c66c84fbecfda8d828089d6c794
katarinahudakova <72190543+katarinahudakova@users.noreply.github.com>
2022-05-29 01:22:52 +0200

    updated footer

--- src/Vue/pages/Footer.vue
+++ src/Vue/pages/Footer.vue
@@ -2,13 +2,12 @@
   <footer class="footer font-small text-center">
     <div class="row">
       <div class="logo">
-        <img src="../../assets/logo.png" alt="" />
-      
+        <img src="../../assets/logo.png" alt="logo" />
 
-      <div class="copyright">
-        <p>Copyright © {{ new Date().getFullYear() }}</p>
+        <div class="copyright">
+          <p>Copyright © {{ new Date().getFullYear() }}</p>
+        </div>
       </div>
-    </div>
     </div>
   </footer>
 </template>
